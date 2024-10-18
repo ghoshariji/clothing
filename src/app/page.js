8cf06a5bbdbabab8b66e9b89702be6b57470048f
ghoshariji <144580430+ghoshariji@users.noreply.github.com>
2024-10-18 09:54:39 +0530

    push

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,18 @@ import Hero from '@/component/Hero'
 
 import React from 'react'
 import { ComplexNavbar } from './_navbar/Navbar'
+import Page from './_footer/Page'
+import HomeProduct from '@/component/HomeProduct'
+import PopUpComponent from '@/component/PopUp'
 
 const page = () => {
   return (
     <div>
       <ComplexNavbar />
       <Hero />
+      <HomeProduct />
+      {/* <PopUpComponent /> */}
+      <Page />
     </div>
   )
 }
